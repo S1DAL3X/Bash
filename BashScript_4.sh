98f4e5d905ead67e96e7b3c818ff587df46f91cf
Aleksey <sidorenkoalekse@mail.ru>
2018-11-28 22:40:40 +0700

    Create BashScript_4.sh

---
+++ BashScript_4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+summ=0
+
+myFunction()
+{
+	echo "This is text from function $0!"
+	echo "num_1 is $1"
+	echo "num_2 is $2"
+	summ=$(($1+$2))
+}
+
+myFunction 200 56
+
+echo "Summ = $summ"
